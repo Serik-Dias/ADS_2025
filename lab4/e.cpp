27e48f53b5f2888788b81172939a2a91a61a5627
Dias <serikdias009@gmail.com>
2025-10-12 14:06:45 +0500

    lab4

--- lab4/e.cpp
+++ lab4/e.cpp
@@ -3,48 +3,45 @@
 #include <queue>
 using namespace std;
 
-struct Node {
-    int val;
-    Node* left;
+class Node{
+    public:
+    int data;
     Node* right;
-    Node(int x) : val(x), left(nullptr), right(nullptr) {}
-};
+    Node* left;
+    Node(int data){
+        this->data = data;
+        right = NULL;
+        left = NULL;
+    }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+};  
+int main(){
     int n;
     cin >> n;
-
-    vector<Node*> nodes(n + 1, nullptr);
-    for (int i = 1; i <= n; i++) {
-        nodes[i] = new Node(i);
+    vector<Node*>vec(n + 1, NULL);
+    for(int i = 1; i <= n; i++){
+        vec[i] = new Node(i);
     }
-
-    for (int i = 0; i < n - 1; i++) {
-        int parent, child, side;
-        cin >> parent >> child >> side;
-        if (side == 0) nodes[parent]->left = nodes[child];
-        else nodes[parent]->right = nodes[child];
+    for(int i = 0; i < n - 1; i++){
+        int x,y,z;
+        cin >> x >> y >> z;
+        if(z == 0)vec[x]->left = vec[y];
+        else vec[x]->right = vec[y];
     }
-
-    queue<Node*> q;
-    q.push(nodes[1]);
-    int maxWidth = 0;
-
-    while (!q.empty()) {
-        int levelSize = q.size();
-        maxWidth = max(maxWidth, levelSize);
-
-        for (int i = 0; i < levelSize; i++) {
-            Node* cur = q.front();
+    Node* root = vec[1];
+    queue<Node*>q;
+    q.push(root);
+    int max_width = 0;
+    while(!q.empty()){
+        int level_size = q.size();
+        max_width = max(max_width, level_size);
+        for(int i = 0; i < level_size; i++){
+            Node*current = q.front();
             q.pop();
-            if (cur->left) q.push(cur->left);
-            if (cur->right) q.push(cur->right);
-        }
+            if(current->right != NULL) q.push(current -> right);
+            if(current->left != NULL) q.push(current -> left);
     }
-
-    cout << maxWidth << "\n";
+}
+    cout << max_width;
     return 0;
 }
